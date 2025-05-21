c39ef6fefe2ed3f9a3190ce9222c3ddcd675c483
Patrick R.H. <patrickrh31@gmail.com>
2025-05-21 11:00:01 +0700

    setup(http): Setup Gin and CORS

---
+++ routes/cors.go
@@ -0,0 +1,16 @@
+package cors
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func CorsMiddleware() gin.HandlerFunc {
+	return cors.New(cors.Config{
+		AllowOrigins: []string{"http://localhost:3000"},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders: []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders: []string{"Content-Length"},
+		AllowCredentials: true,
+	})
+}
